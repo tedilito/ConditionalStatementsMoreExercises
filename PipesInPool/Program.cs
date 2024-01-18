b86f90a0549440720ebe9617a2ea998fdf2f1b8c
Teodor <ti31965162@edu.mon.bg>
2024-01-18 18:18:24 +0200

    Add project files.

---
+++ PipesInPool/Program.cs
@@ -0,0 +1,27 @@
+﻿int volumePool = int.Parse(Console.ReadLine()); // obem
+
+int debit1 = int.Parse(Console.ReadLine()); // debit za 1 chas na truba 1
+int debit2 = int.Parse(Console.ReadLine()); // debit za 1 chas na truba 2
+
+double hoursGone = double.Parse(Console.ReadLine()); // chasove v otsustvie na rabotnika
+
+double debit1Hour = debit1 * hoursGone; // debita za cqlto vreme v otsustvie na rabotnika(truba1)
+double hourDebit2 = debit2 * hoursGone; // debita za cqloto vreme v otsustvie na rabotnika(Truba 2)
+
+double totalDebit = debit1Hour + hourDebit2; //Obshtiqt broi debit
+
+if (totalDebit <= volumePool)
+{
+    double procentZapulvane = (totalDebit / volumePool) * 100;
+    double procentTruba1 = (debit1Hour / totalDebit) * 100;
+    double procentTruba2 = (hourDebit2 / totalDebit) * 100;
+    Console.WriteLine($"The pool is {procentZapulvane}% full. Pipe 1: {procentTruba1}%. Pipe 2: {procentTruba2:f2}%.");
+
+}
+else if (totalDebit > volumePool)
+{
+    double litriPoveche = (totalDebit - volumePool);
+    Console.WriteLine($"For {hoursGone} hours the pool overflows with {litriPoveche:f2} liters.");
+}
+
+
---
+++ SleepyTomCat/Program.cs
@@ -0,0 +1,26 @@
+﻿int countWeekEnd = int.Parse(Console.ReadLine());
+
+int minutesWeekEnd = countWeekEnd * 127;
+int workingDays = (365 - countWeekEnd) * 63;
+int totalPlayTime = minutesWeekEnd + workingDays;
+
+int norma = 30000;
+
+if (norma >= totalPlayTime)
+{
+    int diffNorma = norma - totalPlayTime;
+    int hours = diffNorma / 60;
+    int minutes = diffNorma % 60;
+
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+}
+else
+{
+    int diffNorma = totalPlayTime - norma;
+    int hours = diffNorma / 60;
+    int minute = diffNorma % 60;
+
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine($"{hours} hours and {minute} minutes more for play");
+}
