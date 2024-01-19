4c63d93f761b344b3320f743621b26798a5975e7
tedilito <156572502+tedilito@users.noreply.github.com>
2024-01-19 21:58:31 +0200

    Add files via upload

---
+++ FlowerShop/Program.cs
@@ -0,0 +1,26 @@
+﻿int countMagnolii = int.Parse(Console.ReadLine());
+int countZumbul = int.Parse(Console.ReadLine());    
+int countRozi = int.Parse(Console.ReadLine());
+int countCactus = int.Parse(Console.ReadLine());
+double priceGift = double.Parse(Console.ReadLine());
+
+double priceMagnoliii = countMagnolii * 3.25;
+double priceZumbuli = countZumbul * 4.00;
+double priceRozi = countRozi * 3.50;
+double priceCactus = countCactus * 8.00;
+
+double totalPrice = priceMagnoliii + priceZumbuli + priceRozi + priceCactus;
+double tax = totalPrice * 0.05;
+
+double totalSum = totalPrice - tax;
+
+if (totalSum >= priceGift)
+{
+    double moneyLeft =Math.Floor(totalSum - priceGift);
+    Console.WriteLine($"She is left with {moneyLeft} leva.");
+}
+else if (priceGift > totalSum)
+{
+    double neededMoney = Math.Ceiling(priceGift - totalSum);
+    Console.WriteLine($"She will have to borrow {neededMoney} leva.");
+}
---
+++ FuelTank/Program.cs
@@ -0,0 +1,41 @@
+﻿string typeFuel = Console.ReadLine().ToLower();
+int leeters = int.Parse(Console.ReadLine());
+
+if (typeFuel == "gas" )
+{
+    if (leeters < 25)
+    {
+        Console.WriteLine($"Fill your tank with {typeFuel}!");
+    }
+    else if (leeters >= 25)
+    {
+        Console.WriteLine($"You have enough {typeFuel}.");
+    }
+
+}
+else if (typeFuel == "diesel")
+{
+    if (leeters < 25)
+    {
+        Console.WriteLine($"Fill your tank with {typeFuel}!");
+    }
+    else if (leeters >= 25)
+    {
+        Console.WriteLine($"You have enough {typeFuel}.");
+    }
+}
+else if (typeFuel =="gasoline")
+{
+    if (leeters < 25)
+    {
+        Console.WriteLine($"Fill your tank with {typeFuel}!");
+    }
+    else if (leeters >= 25)
+    {
+        Console.WriteLine($"You have enough {typeFuel}.");
+    }
+}
+else 
+{
+    Console.WriteLine("Invalid fuel!");
+}
